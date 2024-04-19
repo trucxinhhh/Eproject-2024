0b25bb411bd58d5ca8d977142d0461abefface06
trucxinhhh <93142476+trucxinhhh@users.noreply.github.com>
2024-04-19 14:04:10 +0700

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,18 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-n=282
+n=1128
 # Đọc dữ liệu từ file CSV
-# data = pd.read_csv('file_A_test.csv')
-df = pd.read_csv('Y_final.csv')
-
-# Lấy dữ liệu từ cột đó
-# column_data = data['P'].iloc[-n-1:-1]
-column_data = df['0'].iloc[:480]
+data = pd.read_csv('file_A_test_3.csv')
+# df = pd.read_csv('Y_final_P.csv')
+df = pd.read_csv('Y_final_L.csv')
 
 
+
+Y = data['lux'].iloc[-n-1:-1].values
+# Y = data['P'].iloc[-n-1:-1].values
+Y2 = df['0'].iloc[-n-1:-1].values
+# # Y = data['P'].values
+# # Y2 = df['0'].values
+
+print(len(Y2))
+print(len(Y))
 # Vẽ đồ thị
-plt.plot(column_data)
-plt.xlabel('Chỉ số')
-plt.ylabel('Giá trị')
-# plt.title('')
+plt.plot(Y,"b",label= "Thực tế", linewidth=1)
+plt.plot(Y2,"r--",label="Dự đoán",linewidth=2)
 plt.show()

7b9ce5241ae28fa63f07a2baeda1a4f5a42aa5c5
trucxinhhh <93142476+trucxinhhh@users.noreply.github.com>
2024-04-19 14:05:41 +0700

    Update train_RNN.py
    update RNN to LSTM

--- test/train_RNN.py
+++ test/train_RNN.py
@@ -1,27 +1,28 @@
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense,LSTM
 from tensorflow.keras.models import load_model, save_model
 
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
-from TXL import n 
+from TXL import *
 import tensorflow as tf
 import csv
-
+n=282 
 loss=[]
 #B1: chuẩn bị data
 data = pd.read_csv('Book2.csv')
 
 X = (data.iloc[:,3:(n)+3])/100
+# print("len(X)",len(X))
+X = np.expand_dims(X, axis=1) 
 Y=data['P'].values/100
-
-# #B2 chuẩn bị mô hình AI
+print (Y)
+# # #B2 chuẩn bị mô hình AI
 model = Sequential()  #tạo mô hình trống
-model.add(Dense(20, input_shape = (n,), activation='tanh'))
+model.add(LSTM(20, input_shape = (None,n), activation='tanh'))
 model.add(Dense(20, activation ='tanh'))             
-model.add(Dense(2, activation = "linear"))       
+model.add(Dense(1, activation = "linear"))       
 print(model.summary())
 
 
@@ -31,8 +32,8 @@ model.compile(loss='mse', optimizer='Adam')
 
 
 # #B4 huấn luyện mô hình
-history=model.fit(X, Y, epochs=1000, batch_size=32)
-save_model(model, 'NNmodel_3_P.keras')
+history=model.fit(X, Y, epochs=10000, batch_size=32)
+save_model(model, 'NNmodel_13_P.keras')
 
 # Plotting the loss
 plt.plot(history.history['loss'])
@@ -42,24 +43,25 @@ plt.xlabel('Epoch')
 plt.show()
 
 print("Huấn luyện lux")
-X1 = (data.iloc[:,(n)+4:(n)*2+5])/100
+X1 = (data.iloc[:,(n)+4:(n)*2+4])/100
+X1 = np.expand_dims(X1, axis=1)
 Y1=data['Lux'].values/100
 # #B2 chuẩn bị mô hình AI
-model = Sequential()  #tạo mô hình trống
-model.add(Dense(20, input_shape = (n,), activation='tanh'))
-model.add(Dense(20, activation ='tanh'))             
-model.add(Dense(2, activation = "linear"))       
-print(model.summary())
+model1 = Sequential()  #tạo mô hình trống
+model1.add(LSTM(20, input_shape=(None, n), activation='tanh')) 
+model1.add(Dense(10, activation ='tanh'))      
+model1.add(Dense(1, activation = "linear"))       
+print(model1.summary())
 
 
 #B3 chuẩn bị giải thuật huấn luyện mô hình
-model.compile(loss='mse', optimizer='Adam')  
+model1.compile(loss='mse', optimizer='Nadam')  
 
 
 
 # #B4 huấn luyện mô hình
-history=model.fit(X1, Y1, epochs=1000, batch_size=32)
-save_model(model, 'NNmodel_3_Lux.keras')
+history=model1.fit(X1, Y1, epochs=10000, batch_size=32)
+save_model(model1, 'NNmodel_13_Lux.keras')
 
 # Plotting the loss
 plt.plot(history.history['loss'])
